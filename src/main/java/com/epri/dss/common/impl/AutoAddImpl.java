267347ce7ea428ee70714cff56c3d6e702b0b50f
Richard Lincoln <r.w.lincoln@gmail.com>
2011-02-11 12:06:57 +0000

    Implementing CktElementClass.

--- src/main/java/com/epri/dss/common/impl/AutoAddImpl.java
+++ src/main/java/com/epri/dss/common/impl/AutoAddImpl.java
@@ -2,12 +2,25 @@ package com.epri.dss.common.impl;
 
 import java.io.PrintStream;
 
+import com.epri.dss.meter.EnergyMeterObj;
+import com.epri.dss.shared.HashList;
 import com.epri.dss.shared.impl.Complex;
+import com.epri.dss.shared.impl.HashListImpl;
 
 import com.epri.dss.common.AutoAdd;
+import com.epri.dss.common.Circuit;
 import com.epri.dss.conversion.Generator;
 import com.epri.dss.delivery.Capacitor;
+import com.epri.dss.delivery.PDElement;
 
+/**
+ * Unit for processing the AutoAdd Solution functions.
+ * 
+ * Note: Make sure this class in instantiated after energymeter class.
+ * 
+ * There is one of these per circuit.
+ *
+ */
 public class AutoAddImpl implements AutoAdd {
 
 	private Generator GeneratorClass;
@@ -39,12 +52,139 @@ public class AutoAddImpl implements AutoAdd {
 
 	protected boolean ModeChanged;
 
+	private static double sumSelectedRegisters(EnergyMeterObj Mtr, int[] Regs, int count) {
+		double Result = 0.0;
+		for (int i = 0; i < count; i++) {
+			Result += Mtr.getRegisters()[Regs[i]] * Mtr.getTotalsMask()[Regs[i]];
+		}
+		return Result;
+	}
+	
 	public AutoAddImpl() {
-		// TODO Auto-generated constructor stub
+		DSSGlobals Globals = DSSGlobals.getInstance();
+		
+		BusIdxListCreated = false;
+		GeneratorClass = (Generator) Globals.getDSSClassList().get(Globals.getClassNames().find("generator"));
+		CapacitorClass = (Capacitor) Globals.getDSSClassList().get(Globals.getClassNames().find("capacitor"));
+
+		// AutoAdd defaults
+		GenkW   = 1000.0;
+		GenPF   = 1.0;
+		Capkvar = 600.0;
+		AddType = DSSGlobals.GENADD;
+		LastAddedGenerator = 0;
+		LastAddedCapacitor = 0;
+
+		ModeChanged = true;
 	}
 
+	/**
+	 * Make a list of unique bus names.
+	 * If AutoAddBusList in ActiveCircuit is not nil, use this list.
+	 * Else, Use the element lists in Energy Meters.
+	 * If no Energy Meters, use all the buses in the active circuit.
+	 */
+	public void makeBusList() {
+		int retval;
+		String Bname;
+		PDElement PDElem;
+		HashList BusList;
+		boolean BusListCreatedHere;
+
+		if (BusIdxListCreated)
+			BusIdxList = new int[0];
+
+		BusListCreatedHere = false;
+		BusIdxListCreated = false;
+		
+		DSSGlobals Globals = DSSGlobals.getInstance();
+
+		// Autoaddbuslist exists in Active Circuit, use it  (see set Autobuslist=)
+		if (Globals.getActiveCircuit().getAutoAddBusList().listSize() > 0) {
+			BusList = Globals.getActiveCircuit().getAutoAddBusList();
+		} else  {
+			if (Globals.getActiveCircuit().getEnergyMeters().listSize() = 0) {
+				// No energymeters in circuit
+				// Include all buses in the circuit
+				BusIdxListSize = Globals.getActiveCircuit().getBusList().listSize();
+				BusIdxList = (int[]) Utilities.resizeArray(BusIdxList, BusIdxListSize);
+
+				for (int i = 0; i < BusIdxListSize; i++) 
+					BusIdxList[i] = i;
+
+				BusIdxListCreated = true;
+				return;
+			} else {
+				/* Construct Bus List from Energy Meters Zone Lists */
+				// Include only buses in EnergyMeter lists
+				// Consider all meters
+				BusListCreatedHere = true;
+				BusList = new HashListImpl(Globals.getActiveCircuit().getNumBuses());
+				for (EnergyMeterObj pMeter : Globals.getActiveCircuit().getEnergyMeters()) {
+					if (pMeter.getBranchList() != null) {
+						PDElem = pMeter.BranchList.getFirst();
+						while (PDElem != null) {  // add only unique bus names
+							for (int i = 0; i < PDElem.getNTerms(); i++) {
+								Bname = Utilities.stripExtension(PDElem.getBus(i));
+								retval = BusList.find(Bname);
+								if (retval == -1) {  // TODO Check zero based indexing
+									BusList.add(Bname);  // return value is index of bus
+								}
+							}
+							PDElem = (PDElement) pMeter.getBranchList().GoForward();
+						}
+					}
+				}
+			}
+		}
+
+		// Make busIdxList from BusList
+		BusIdxListSize = BusList.listSize();
+		BusIdxList = (int[]) Utilities.resizeArray(BusIdxList, BusIdxListSize);
+
+		for (int i = 0; i < BusIdxListSize; i++) {
+			BusIdxList[i] = Globals.getActiveCircuit().getBusList().find(BusList.get(i));
+		}
+
+		if (BusListCreatedHere)
+			BusList = null;
+		BusIdxListCreated = true;
+	}
+
+	/**
+	 * Returns losses in metered part of circuit + weighted EEN values.
+	 * 
+	 * If no meters, returns just total losses in circuit.
+	 * 
+	 * Base everything on gen kW.
+	 */
 	private double getWeightedLosses() {
-		return 0.0;
+		double Result;
+
+		ComputekWLosses_EEN();
+
+		if (DSSGlobals.getInstance().getActiveCircuit().getEnergyMeters().size() == 0) {
+			// No energymeters in circuit
+			// Just go by total system losses
+			puLossImprovement = (BaseLosses - kWLosses) / GenkW;
+			puEENImprovement = 0.0;
+			Result = puLossImprovement;
+		} else {
+			Circuit ckt = DSSGlobals.getInstance().getActiveCircuit();
+			
+			puLossImprovement = (BaseLosses - kWLosses) / GenkW;
+			puEENImprovement = (BaseEEN - kWEEN)/GenkW;
+			Result = ckt.getLossWeight() * puLossImprovement + ckt.getUEWeight() * puEENImprovement;
+		}
+		return Result;
+	}
+
+	public void appendToFile(String WhichFile, String S) {
+
+	}
+
+	public void addCurrents(int SolveType) {
+
 	}
 
 	private void ComputekWLosses_EEN() {
@@ -109,18 +249,6 @@ public class AutoAddImpl implements AutoAdd {
 
 	public void setModeChanged(boolean modeChanged) {
 		ModeChanged = modeChanged;
-	}
-
-	public void makeBusList() {
-
-	}
-
-	public void appendToFile(String WhichFile, String S) {
-
-	}
-
-	public void addCurrents(int SolveType) {
-
 	}
 
 }
--- src/main/java/com/epri/dss/common/impl/CktElementClassImpl.java
+++ src/main/java/com/epri/dss/common/impl/CktElementClassImpl.java
@@ -1,34 +1,68 @@
 package com.epri.dss.common.impl;
 
+import com.epri.dss.common.CktElement;
 import com.epri.dss.common.CktElementClass;
+import com.epri.dss.parser.impl.Parser;
 
 public class CktElementClassImpl extends DSSClassImpl implements
 		CktElementClass {
 
+	private int NumCktElemClassProps;
+
 	public CktElementClassImpl() {
-		// TODO Auto-generated constructor stub
+		super();
+		this.NumCktElemClassProps = 2;
 	}
 
 	protected int classEdit(Object ActiveCktElemObj, int ParamPointer) {
+		// continue parsing with contents of Parser
+		if (ParamPointer >= 0) {  // TODO Check zero based indexing
+			CktElement cktElem = DSSCktElement(ActiveCktElemObj); // FIXME Translate this
+			switch (ParamPointer) {
+			case 0:
+				cktElem.setBaseFrequency(Parser.getInstance().makeDouble());
+			case 2:
+				cktElem.setEnabled(Utilities.interpretYesNo(Parser.getInstance().makeString()));
+			default:
+				super.classEdit(ActiveCktElemObj, ParamPointer - NumCktElemClassProps);
+			}
+		}
 		return 0;
 	}
 
 	protected void classMakeLike(Object OtherObj) {
+		CktElement OtherCktObj = DSSCktElement(OtherObj);  // TODO Translate this
 
+		CktElement cktElem = DSSCktElement(DSSGlobals.getInstance().getActiveDSSObject());  // TODO Translate this
+		cktElem.setBaseFrequency(OtherCktObj.getBaseFrequency());
+		cktElem.setEnabled(true);
 	}
 
-	/* Add no. of intrinsic properties */
+	/**
+	 * Add no. of intrinsic properties.
+	 */
 	protected void countProperties() {
-
+		NumProperties = NumProperties + NumCktElemClassProps;
+		super.countProperties();
 	}
 
-	/* Add Properties of this class to propName */
+	/**
+	 * Define the properties for the base power delivery element class.
+	 */
 	protected void defineProperties() {
+		PropertyName[ActiveProperty + 1] = "basefreq";  // TODO Check zero based indexing
+		PropertyName[ActiveProperty + 2] = "enabled";
 
+		PropertyHelp[ActiveProperty + 1] = "Base Frequency for ratings.";
+		PropertyHelp[ActiveProperty + 2] = "{Yes|No or True|False} Indicates whether this element is enabled.";
+
+		ActiveProperty = ActiveProperty + NumCktElemClassProps;
+
+		super.defineProperties();
 	}
 
-	public int numCktElemClassProps() {
-		return 0;
+	public int getNumCktElemClassProps() {
+		return NumCktElemClassProps;
 	}
 
 }
