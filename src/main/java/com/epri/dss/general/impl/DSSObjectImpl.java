937402dc05941353fa63be3cdf0708a02fa234f0
Richard Lincoln <r.w.lincoln@gmail.com>
2011-02-19 15:21:26 +0000

    Reordering DSSObject methods.

--- src/main/java/com/epri/dss/general/impl/DSSObjectImpl.java
+++ src/main/java/com/epri/dss/general/impl/DSSObjectImpl.java
@@ -13,6 +13,7 @@ import java.io.PrintStream;
 
 import com.epri.dss.common.DSSClass;
 import com.epri.dss.common.impl.DSSClassImpl;
+import com.epri.dss.common.impl.Utilities;
 import com.epri.dss.general.DSSObject;
 
 public class DSSObjectImpl extends NamedObjectImpl implements DSSObject {
@@ -36,6 +37,12 @@ public class DSSObjectImpl extends NamedObjectImpl implements DSSObject {
 	/* General purpose Flag for each object  don't assume inited */
 	protected boolean Flag;
 
+	public void clearPropSeqArray() {
+		PropSeqCount = 0;
+		for (int i = 0; i < ParentClass.getNumProperties(); i++) {
+			PrpSequence[i] = 0;
+		}
+	}
 
 	public DSSObjectImpl(DSSClass ParClass) {
 		super(ParClass.getName());
@@ -59,6 +66,89 @@ public class DSSObjectImpl extends NamedObjectImpl implements DSSObject {
 		PrpSequence = new int[0];
 
 		super.finalize();
+	}
+
+	public void dumpProperties(PrintStream F, boolean Complete) {
+		F.println();
+		F.println("New " + getDSSClassName() + '.' + getName());
+	}
+
+	/**
+	 * Allow calls to edit from object itself.
+	 */
+	public int edit() {
+		ParentClass.setActive(ClassIndex);
+		return ParentClass.edit();
+	}
+
+	/**
+	 * Use DSSClass.propertyIndex() to get index by name.
+	 */
+	public String getPropertyValue(int Index) {
+		// Default behavior for all DSS Objects
+		return this.PropertyValue[Index];
+	}
+
+	public void initPropertyValues(int ArrayOffset) {
+		this.PropertyValue[ArrayOffset] = "";
+
+		// Clear propertySequence Array after initialization
+		clearPropSeqArray();
+	}
+
+	public void saveWrite(PrintStream F) {
+		/* Write only properties that were explicitly set in the
+		 * final order they were actually set.
+		 */
+		int iProp = getNextPropertySet(0); // Works on ActiveDSSObject
+		while (iProp > 0) {
+			DSSClass pc = ParentClass;
+			F.print(" " + pc.getPropertyName().get(pc.getRevPropertyIdxMap()[iProp]));
+			F.print("=" + Utilities.checkForBlanks(PropertyValue[iProp]));
+			iProp = getNextPropertySet(iProp);
+		}
+	}
+
+	/**
+	 * Find next larger property sequence number
+	 * return 0 if none found
+	 */
+	protected int getNextPropertySet(int idx) {
+		int Smallest = 9999999; // some big number
+		int Result = 0;
+
+		if (idx > 0)
+			idx = PrpSequence[idx];
+
+		for (int i = 0; i < ParentClass.getNumProperties(); i++) 
+			if (PrpSequence[i] > idx) 
+				if (PrpSequence[i] < Smallest) {
+					Smallest = PrpSequence[i];
+					Result = i;
+				}
+
+		return Result;
+	}
+
+	public void setName(String Value) {
+		// If renamed, then let someone know so hash list can be updated
+		if (getLocalName().length() > 0)
+			ParentClass.setElementNamesOutOfSynch(true);
+
+		setLocalName(Value);
+	}
+
+	public void setPropertyValue(int Index, String Value) {
+		PropertyValue[Index] = Value;
+
+		// Keep track of the order in which this property was
+		// accessed for Save Command
+		PropSeqCount += 1;
+		PrpSequence[Index] = PropSeqCount;
+	}
+
+	public String getName() {
+		return getLocalName();
 	}
 
 	public int getDSSObjType() {
@@ -100,92 +190,5 @@ public class DSSObjectImpl extends NamedObjectImpl implements DSSObject {
 	public void setFlag(boolean flag) {
 		Flag = flag;
 	}
-
-	/* Use DSSClass.PropertyIndex to get index by name */
-	public String getPropertyValue(int Index) {
-		// Default Behavior for all DSS Objects
-		return this.PropertyValue[Index];
-	}
-
-	public void setPropertyValue(int Index, String Value) {
-		PropertyValue[Index] = Value;
-
-		// Keep track of the order in which this property was
-		// accessed for Save Command
-		PropSeqCount += 1;
-		PrpSequence[Index] = PropSeqCount;
-	}
-
-	public String getName() {
-		return getLocalName();
-	}
-
-	public void setName(String Value) {
-		// If renamed, then let someone know so hash list can be updated
-		if (getLocalName().length() > 0)
-			ParentClass.setElementNamesOutOfSynch(true);
-
-		setLocalName(Value);
-	}
-
-	/*
-	 * Find next larger property sequence number
-	 * return 0 if none found
-	 */
-	protected int GetNextPropertySet(int idx) {
-		int Smallest = 9999999; // some big number
-		int Result = 0;
-
-		if (idx > 0)
-			idx = PrpSequence[idx];
-
-		for (int i = 0; i < ParentClass.getNumProperties(); i++) {
-			if (PrpSequence[i] > idx) {
-				if (PrpSequence[i] < Smallest) {
-					Smallest = PrpSequence[i];
-					   Result = i;
-				}
-			}
-		}
-
-		return Result;
-	}
-
-	/* Allow Calls to edit from object itself */
-	public int edit() {
-		ParentClass.setActive(ClassIndex);
-		return 0;//super.edit();
-	}
-
-	public void initPropertyValues(int ArrayOffset) {
-		this.PropertyValue[ArrayOffset] = "";
-
-		// Clear propertySequence Array after initialization
-		clearPropSeqArray();
-	}
-
-	public void dumpProperties(PrintStream F, boolean Complete) {
-		F.println();
-		F.println("New " + getDSSClassName() + '.' + getName());
-	}
-
-	public void saveWrite(PrintStream F) {
-		/* Write only properties that were explicitly set in the
-		final order they were actually set */
-		int iProp = GetNextPropertySet(0); // Works on ActiveDSSObject
-		while (iProp > 0) {
-			DSSClass pc = ParentClass;
-//			F.print(' ' + pc.getPropertyName(pc.getRevPropertyIdxMap()[iProp]));
-//			F.print('=' + Utilities.CheckForBlanks(PropertyValue[iProp]));
-			iProp = GetNextPropertySet(iProp);
-		}
-	}
-
-	public void clearPropSeqArray() {
-		PropSeqCount = 0;
-		for (int i = 0; i < ParentClass.getNumProperties(); i++) {
-			PrpSequence[i] = 0;
-		}
-	}
-
+	
 }
--- src/main/java/com/epri/dss/general/impl/GrowthShapeImpl.java
+++ src/main/java/com/epri/dss/general/impl/GrowthShapeImpl.java
@@ -1,20 +1,159 @@
 package com.epri.dss.general.impl;
 
+import com.epri.dss.common.Circuit;
+import com.epri.dss.common.impl.DSSClassDefs;
 import com.epri.dss.common.impl.DSSClassImpl;
+import com.epri.dss.common.impl.DSSGlobals;
+import com.epri.dss.common.impl.Utilities;
 import com.epri.dss.general.GrowthShape;
+import com.epri.dss.general.GrowthShapeObj;
+import com.epri.dss.parser.impl.Parser;
+import com.epri.dss.shared.impl.CommandListImpl;
 
 public class GrowthShapeImpl extends DSSClassImpl implements GrowthShape {
+	
+	private GrowthShapeObj ActiveGrowthShapeObj;
 
 	public GrowthShapeImpl() {
-		// TODO Auto-generated constructor stub
+		super();
+		this.Class_Name = "GrowthShape";
+		this.DSSClassType = DSSClassDefs.DSS_OBJECT;
+
+		this.ActiveElement = 0;
+
+		defineProperties();
+
+		String[] Commands = new String[0];
+		System.arraycopy(this.PropertyName, 0, Commands, 0, this.NumProperties);
+		this.CommandList = new CommandListImpl(Commands);
+		this.CommandList.setAbbrevAllowed(false);
 	}
 
-	/* Returns active GrowthShape string */
+	protected void defineProperties() {
+		String CRLF = DSSGlobals.CRLF;
+		
+		NumProperties = GrowthShape.NumPropsThisClass;
+		countProperties();   // Get inherited property count
+
+		allocatePropertyArrays();
+
+
+		// Define Property names
+
+		PropertyName[0] = "npts";     // Number of points to expect
+		PropertyName[1] = "year";     // vextor of year values
+		PropertyName[2] = "mult";     // vector of multiplier values corresponding to years
+		PropertyName[3] = "csvfile";  // Switch input to a csvfile                 (year, mult)
+		PropertyName[4] = "sngfile";  // switch input to a binary file of singles  (year, mult)
+		PropertyName[5] = "dblfile";  // switch input to a binary file of doubles (year, mult)
+
+		PropertyHelp[0] = "Number of points to expect in subsequent vector.";
+		PropertyHelp[1] = "Array of year values, or a text file spec, corresponding to the multipliers. "+
+				"Enter only those years where the growth changes. "+
+				"May be any integer sequence -- just so it is consistent. See help on Mult.";
+		PropertyHelp[2] = "Array of growth multiplier values, or a text file spec, corresponding to the year values. "+
+				"Enter the multiplier by which you would multiply the previous year's load to get the present year's."+
+				CRLF+CRLF+"Examples:"+CRLF+CRLF+
+				"  Year = [1, 2, 5]   Mult=[1.05, 1.025, 1.02]."+CRLF+
+				"  Year= (File=years.txt) Mult= (file=mults.txt)."+ CRLF+CRLF+
+				"Text files contain one value per line.";
+		PropertyHelp[3] = "Switch input of growth curve data to a csv file containing (year, mult) points, one per line.";
+		PropertyHelp[4] = "Switch input of growth curve data to a binary file of singles "+
+				"containing (year, mult) points, packed one after another.";
+		PropertyHelp[5] = "Switch input of growth curve data to a binary file of doubles "+
+				"containing (year, mult) points, packed one after another.";
+
+		ActiveProperty = GrowthShape.NumPropsThisClass;
+		super.defineProperties();  // Add defs of inherited properties to bottom of list
+	}
+
+	public int newObject(String ObjName) {
+		// Create a new object of this class and add to list.
+		DSSGlobals Globals = DSSGlobals.getInstance();
+		Globals.setActiveDSSObject(new GrowthShapeObjImpl(this, ObjName));
+		return addObjectToList(Globals.getActiveDSSObject());
+	}
+
+	public int edit() {
+		DSSGlobals Globals = DSSGlobals.getInstance();
+		
+		double[] YrBuffer;
+
+		int Result = 0;
+		// continue parsing with contents of parser
+		setActiveGrowthShapeObj(ElementList.getActive());
+		Globals.setActiveDSSObject((DSSObjectImpl) getActiveGrowthShapeObj());
+
+		GrowthShapeObj pShape = getActiveGrowthShapeObj();
+
+		int ParamPointer = 0;
+		String ParamName = Parser.getInstance().getNextParam();
+		String Param = Parser.getInstance().makeString();
+		while (Param.length() > 0) {
+			if (ParamName.length() == 0) {
+				ParamPointer += 1;
+			} else {
+				ParamPointer = CommandList.getCommand(ParamName);
+			}
+	
+			if ((ParamPointer > 0) && (ParamPointer <= NumProperties))  // TODO Check zero based indexing
+				pShape.setPropertyValue(ParamPointer, Param);
+
+			switch (ParamPointer) {
+			case 0:
+				Globals.doSimpleMsg("Unknown parameter \"" + ParamName + "\" for Object \"" + Class_Name +"."+ getName() + "\"", 600);
+			case 1:
+				Npts = Parser.getInstance().makeInteger();
+			case 2:
+				Year = Utilities.resizeArray(Year, Npts);
+				YrBuffer = new double[Npts];
+				Utilities.interpretDblArray(Param, Npts, YrBuffer);  // Parser.parseAsVector(Npts, Yrbuffer);
+					
+				for (int i = 0; i < Npts; i++) 
+					Year[i] = Math.round(YrBuffer[i]);
+				BaseYear = Year[0];
+				YrBuffer = null;
+			case 3: 
+				Multiplier = Utilities.resizeArray(Multiplier, Npts);
+				Utilities.interpretDblArray(Param, Npts, Multiplier);   //Parser.parseAsVector(Npts, Multiplier);
+			case 4:
+				doCSVFile(Param);
+			case 5:
+				doSngFile(Param);
+			case 6:
+				doDblFile(Param);
+			default:
+				// Inherited parameters
+				classEdit(getActiveGrowthShapeObj(), ParamPointer - GrowthShape.NumPropsThisClass);
+			}
+
+			ParamName = Parser.getInstance().getNextParam();
+			Param = Parser.getInstance().makeString();
+		}
+
+		pShape.reCalcYearMult();
+	
+		return Result;
+	}
+
+	public GrowthShapeObj getActiveGrowthShapeObj() {
+		return ActiveGrowthShapeObj;
+	}
+
+	public void setActiveGrowthShapeObj(GrowthShapeObj activeGrowthShapeObj) {
+		ActiveGrowthShapeObj = activeGrowthShapeObj;
+	}
+
+	/**
+	 * Returns active GrowthShape string.
+	 */
 	public String getCode() {
 		return null;
 	}
 
-	/* Sets the active GrowthShape */
+	/**
+	 * Sets the active GrowthShape.
+	 */
 	public void setCode(String Value) {
 
 	}
@@ -31,23 +170,11 @@ public class GrowthShapeImpl extends DSSClassImpl implements GrowthShape {
 
 	}
 
-	protected void defineProperties() {
-
-	}
-
 	protected int makeLike(String ShapeName) {
 		return 0;
 	}
 
-	public int edit() {
-		return 0;
-	}
-
 	public int init(int Handle) {
-		return 0;
-	}
-
-	public int newObject(String ObjName) {
 		return 0;
 	}
 
