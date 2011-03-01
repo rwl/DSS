fa370512bf16883e36e3ccb7e7b57efeba4211df
Richard Lincoln <r.w.lincoln@gmail.com>
2011-03-01 15:52:33 +0000

    Implementing WireDataObj.

--- src/main/java/com/epri/dss/general/impl/WireDataObjImpl.java
+++ src/main/java/com/epri/dss/general/impl/WireDataObjImpl.java
@@ -3,7 +3,9 @@ package com.epri.dss.general.impl;
 import java.io.PrintStream;
 
 import com.epri.dss.common.DSSClass;
+import com.epri.dss.general.WireData;
 import com.epri.dss.general.WireDataObj;
+import com.epri.dss.shared.impl.LineUnits;
 
 public class WireDataObjImpl extends DSSObjectImpl implements WireDataObj {
 	
@@ -20,7 +22,68 @@ public class WireDataObjImpl extends DSSObjectImpl implements WireDataObj {
 
 	public WireDataObjImpl(DSSClass ParClass, String WireDataName) {
 		super(ParClass);
-		// TODO Auto-generated constructor stub
+		setName(WireDataName.toLowerCase());
+		this.DSSObjType = ParClass.getDSSClassType();
+
+		this.RDC              = -1.0;
+		this.R60              = -1.0;
+		this.GMR60            = -1.0;
+		this.radius           = -1.0;
+		this.GMRUnits         = 0;
+		this.ResistanceUnits  = 0;
+		this.RadiusUnits      = 0;
+
+		this.NormAmps    = -1.0;
+		this.EmergAmps   =-1.0;
+
+		initPropertyValues(0);
+	}
+	
+	@Override
+	public void dumpProperties(PrintStream F, boolean Complete) {
+		super.dumpProperties(F, Complete);
+
+		for (int i = 0; i < ParentClass.getNumProperties(); i++) {
+			F.print("~ " + ParentClass.getPropertyName()[i] + "=");
+			switch (i) {
+			case 0:
+				F.println(String.format("%.6g", getRDC()));
+			case 1:
+				F.println(String.format("%.6g", getR60()));
+			case 2:
+				F.println(String.format("%s", LineUnits.lineUnitsStr(getResistanceUnits())));
+			case 3:
+				F.println(String.format("%.6g", getGMR60()));
+			case 4:
+				F.println(String.format("%s", LineUnits.lineUnitsStr(getGMRUnits())));
+			case 5:
+				F.println(String.format("%.6g", getRadius()));
+			case 6:
+				F.println(String.format("%s", LineUnits.lineUnitsStr(getRadiusUnits())));
+			case 7:
+				F.println(String.format("%.6g", getNormAmps()));
+			case 8:
+				F.println(String.format("%.6g", getEmergAmps()));
+			case 9:
+				F.println(String.format("%.6g", getRadius() * 2.0));
+			}
+		}
+	}
+	
+	@Override
+	public void initPropertyValues(int ArrayOffset) {
+		PropertyValue[1] = "-1";
+		PropertyValue[2] =  "-1";
+		PropertyValue[3] =  "none";
+		PropertyValue[4] =  "-1";
+		PropertyValue[5] =  "none";
+		PropertyValue[6] =  "-1";
+		PropertyValue[7] =  "none";
+		PropertyValue[8] =  "-1";
+		PropertyValue[9] =  "-1";
+		PropertyValue[10] =  "-1";
+
+		super.initPropertyValues(WireData.NumPropsThisClass);		
 	}
 
 	public double getNormAmps() {
@@ -65,16 +128,6 @@ public class WireDataObjImpl extends DSSObjectImpl implements WireDataObj {
 
 	public int getRadiusUnits() {
 		return RadiusUnits;
-	}
-	
-	@Override
-	public void initPropertyValues(int ArrayOffset) {
-		
-	}
-	
-	@Override
-	public void dumpProperties(PrintStream F, boolean Complete) {
-		
 	}
 	
 	// FIXME Private members in OpenDSS
