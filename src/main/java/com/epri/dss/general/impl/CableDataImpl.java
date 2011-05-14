b7bdd458be14b755a99a1d20e493e430bdd2b02d
Richard Lincoln <rwl@thinker.cable.virginmedia.net>
2011-05-14 12:02:49 +0100

    Implementing CableData.

--- src/main/java/com/epri/dss/general/impl/CableDataImpl.java
+++ src/main/java/com/epri/dss/general/impl/CableDataImpl.java
@@ -1,26 +1,93 @@
 package com.epri.dss.general.impl;
 
+import com.epri.dss.common.impl.DSSClassDefs;
+import com.epri.dss.common.impl.DSSGlobals;
 import com.epri.dss.general.CableData;
+import com.epri.dss.general.CableDataObj;
 import com.epri.dss.general.DSSObject;
+import com.epri.dss.parser.impl.Parser;
 
 public class CableDataImpl extends ConductorDataImpl implements CableData {
 
 	private int NumCableClassProps;
 
-	protected void countProperties() {
+	public CableDataImpl() {
+		this.NumCableClassProps = 4;
+		this.DSSClassType = DSSClassDefs.DSS_OBJECT;
+	}
 
+	protected void countProperties() {
+		NumProperties = NumProperties + NumCableClassProps;
+		super.countProperties();
 	}
 
 	protected void defineProperties() {
+		PropertyName[ActiveProperty + 0] = "EpsR";
+		PropertyName[ActiveProperty + 1] = "InsLayer";
+		PropertyName[ActiveProperty + 2] = "DiaIns";
+		PropertyName[ActiveProperty + 3] = "DiaCable";
 
+		PropertyHelp[ActiveProperty + 0] = "Insulation layer relative permittivity; default is 2.3.";
+		PropertyHelp[ActiveProperty + 1] = "Insulation layer thickness; same units as radius; no default.";
+		PropertyHelp[ActiveProperty + 2] = "Diameter over insulation layer; same units as radius; no default.";
+		PropertyHelp[ActiveProperty + 3] = "Diameter over cable; same units as radius; no default.";
+
+		ActiveProperty = ActiveProperty + NumCableClassProps;
+		super.defineProperties();
 	}
 
 	protected int classEdit(DSSObject ActiveObj, int ParamPointer) {
-		return 0;
+		DSSGlobals Globals = DSSGlobals.getInstance();
+		Parser parser = Parser.getInstance();
+
+		int Result = 0;
+		// continue parsing with contents of parser
+		if (ParamPointer >= 0) {
+			CableDataObj cd = (CableDataObj) ActiveObj;
+			switch (ParamPointer) {
+			case 0:
+				cd.setEpsR(parser.makeDouble());
+			case 1:
+				cd.setInsLayer(parser.makeDouble());
+			case 2:
+				cd.setDiaIns(parser.makeDouble());
+			case 3:
+				cd.setDiaCable(parser.makeDouble());
+			default:
+				super.classEdit(ActiveObj, ParamPointer - NumCableClassProps);
+			}
+			/* Check for critical errors */
+			switch (ParamPointer) {
+			case 0:
+				if (cd.getEpsR() < 1.0)
+					Globals.doSimpleMsg("Error: Insulation permittivity must be greater than one for CableData " + cd.getName(), 999);
+			case 1:
+				if (cd.getInsLayer() <= 0.0)
+					Globals.doSimpleMsg("Error: Insulation layer thickness must be positive for CableData " + cd.getName(), 999);
+			case 2:
+				if (cd.getDiaIns() <= 0.0)
+					Globals.doSimpleMsg("Error: Diameter over insulation layer must be positive for CableData " + cd.getName(), 999);
+			case 3:
+				if (cd.getDiaCable() <= 0.0)
+					Globals.doSimpleMsg("Error: Diameter over cable must be positive for CableData " + cd.getName(), 999);
+			}
+		}
+
+		return Result;
 	}
 
 	protected void classMakeLike(DSSObject OtherObj) {
+		DSSGlobals Globals = DSSGlobals.getInstance();
 
+		CableDataObj OtherCableData = (CableDataObj) OtherObj;
+		CableDataObj cd = (CableDataObj) Globals.getActiveDSSObject();
+
+		cd.setEpsR(OtherCableData.getEpsR());
+		cd.setInsLayer(OtherCableData.getInsLayer());
+		cd.setDiaIns(OtherCableData.getDiaIns());
+		cd.setDiaCable(OtherCableData.getDiaCable());
+
+		super.classMakeLike(OtherObj);
 	}
 
 	public void setNumCableClassProps(int numCableClassProps) {
--- src/main/java/com/epri/dss/general/impl/CableDataObjImpl.java
+++ src/main/java/com/epri/dss/general/impl/CableDataObjImpl.java
@@ -15,17 +15,42 @@ public class CableDataObjImpl extends ConductorDataObjImpl implements CableDataO
 
 	public CableDataObjImpl(DSSClass ParClass, String CableDataName) {
 		super(ParClass, CableDataName);
-		// TODO Auto-generated constructor stub
-	}
 
-	@Override
-	public void initPropertyValues(int ArrayOffset) {
+		setName(CableDataName.toLowerCase());
+		this.DSSObjType = ParClass.getDSSClassType();
 
+		this.EpsR     =  2.3;
+		this.InsLayer = -1.0;
+		this.DiaIns   = -1.0;
+		this.DiaCable = -1.0;
 	}
 
 	@Override
 	public void dumpProperties(PrintStream F, boolean Complete) {
+		super.dumpProperties(F, Complete);
 
+		for (int i = 0; i < getParentClass().getNumProperties(); i++) {
+			F.print("~ " + getParentClass().getPropertyName()[i] + "=");
+			switch (i) {
+			case 0:
+				F.println(String.format("%.3g", EpsR));
+			case 1:
+				F.println(String.format("%.6g", InsLayer));
+			case 2:
+				F.println(String.format("%.6g", DiaIns));
+			case 3:
+				F.println(String.format("%.6g", DiaCable));
+			}
+		}
+	}
+
+	@Override
+	public void initPropertyValues(int ArrayOffset) {
+		setPropertyValue(ArrayOffset + 0, "2.3");
+		setPropertyValue(ArrayOffset + 1, "-1");
+		setPropertyValue(ArrayOffset + 2, "-1");
+		setPropertyValue(ArrayOffset + 3, "-1");
+		super.initPropertyValues(ArrayOffset + 4);
 	}
 
 	public double getEpsR() {
@@ -42,6 +67,24 @@ public class CableDataObjImpl extends ConductorDataObjImpl implements CableDataO
 
 	public double getDiaCable() {
 		return DiaCable;
+	}
+
+	// FIXME Private members in OpenDSS.
+
+	public void setEpsR(double epsR) {
+		EpsR = epsR;
+	}
+
+	public void setInsLayer(double insLayer) {
+		InsLayer = insLayer;
+	}
+
+	public void setDiaIns(double diaIns) {
+		DiaIns = diaIns;
+	}
+
+	public void setDiaCable(double diaCable) {
+		DiaCable = diaCable;
 	}
 
 }
