747bc0a231f99a8802f63dfcb7e39a34765fc67d
Richard Lincoln <r.w.lincoln@gmail.com>
2011-08-15 16:19:11 +0100

    Adding main entry point to test package.

---
+++ src/test/java/com/epri/dss/parser/Main.java
@@ -0,0 +1,21 @@
+package com.epri.dss.parser;
+
+import com.epri.dss.executive.Executive;
+import com.epri.dss.executive.impl.DSSExecutive;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Executive ex = DSSExecutive.getInstance();
+
+		String cmd = "Compile (/home/rwl/tmp/OpenDSS/Test/IEEE13_LineSpacing.dss)";
+
+		ex.setCommand(cmd);
+
+	}
+
+}
