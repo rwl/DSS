443b495e283ce138a3a1684cc121e0f4ca12dcef
rwl <r.w.lincoln@gmail.com>
2012-03-19 00:16:05 +0000

    Adding JLine shell project.

---
+++ shell-jline/src/main/java/com/ncond/dss/shell/jline/AbstractShell.java
@@ -0,0 +1,68 @@
+package com.ncond.dss.shell.jline;
+
+import java.io.IOException;
+
+import jline.ANSIBuffer;
+import jline.ConsoleReader;
+
+public abstract class AbstractShell implements Shell {
+
+	public static String promptString = ">";
+
+	protected String shellPrompt = promptString + " ";
+
+	protected ConsoleReader reader;
+
+	public void run() {
+
+		try {
+			reader = new ConsoleReader();
+		} catch (IOException e) {
+			throw new IllegalStateException("Failure starting console reader", e);
+		}
+
+		reader.addCompletor(new DSSCompletor());
+
+		setPromptBase(null);
+
+		promptLoop();
+	}
+
+	public void promptLoop() {
+		String line;
+		try {
+			while ((line = reader.readLine(shellPrompt)) != null)
+				if (!line.equals(""))
+					executeCommand(line);
+		} catch (IOException e) {
+			throw new IllegalStateException("JLine error", e);
+		}
+	}
+
+	public abstract void executeCommand(String line);
+
+	public void setPromptBase(String base) {
+		if (reader.getTerminal().isANSISupported()) {
+			ANSIBuffer ansi = new ANSIBuffer();
+			if (base == null || base.equals("")) {
+				shellPrompt = ansi.yellow(promptString + " ").toString();
+			} else {
+				shellPrompt = ansi.cyan(base).yellow(" " + promptString + " ").toString();
+			}
+		} else {
+			if (base == null || base.equals("")) {
+				shellPrompt = promptString + " ";
+			} else {
+				shellPrompt = base + " " + promptString + " ";
+			}
+		}
+	}
+
+	public String getShellPrompt() {
+		return shellPrompt;
+	}
+
+	public static void setPromptString(String promptString) {
+		AbstractShell.promptString = promptString;
+	}
+}
---
+++ shell-jline/src/main/java/com/ncond/dss/shell/jline/Main.java
@@ -0,0 +1,10 @@
+package com.ncond.dss.shell.jline;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Shell dssShell = new DSSShell();
+		dssShell.run();
+	}
+
+}
