8edc428d7f8751c826e5e3d5c55d5d5e0e8599b1
rwl <r.w.lincoln@gmail.com>
2012-03-21 05:11:15 +0000

    Setting ANSI console property.

--- shell-roo/dss.sh
+++ shell-roo/dss.sh
@@ -2,4 +2,4 @@
 
 export MAVEN_OPTS=-Xmx256m
 
-mvn exec:java -Dexec.mainClass="com.ncond.dss.shell.roo.DSSShell"
+mvn exec:java -Dexec.mainClass="com.ncond.dss.shell.roo.DSSShell" -Droo.console.ansi=true
